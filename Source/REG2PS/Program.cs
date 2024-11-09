f9bbc8499df09b30838a46c840e77d265727e3e9
Tom Plant <tom@tplant.com.au>
2024-11-10 00:06:03 +1100

    Replace System.Web with CoreWebForms
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/pl4nty/Reg2CI?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ Source/REG2PS/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSystemWebAdapters()
+    .AddPreApplicationStartMethod(false) // Used if you want to run any pre application start methods
+    .AddJsonSessionSerializer()
+    .AddWrappedAspNetCoreSession()
+    .AddRouting()
+    .AddWebForms()
+    .AddScriptManager() // Remove if you don't use ScriptManager/AJAX
+    .AddOptimization() // Remove if you don't use System.Web.Optimization
+    .AddDynamicPages() // Used if you have dynamic pages
+    .AddCompiledPages(); // Used if compiled pages are used
+
+builder.Services.AddSession();
+builder.Services.AddDistributedMemoryCache();
+
+var app = builder.Build();
+
+foreach (var staticPath in new[] { "Content", "Scripts" })
+{
+    app.UseStaticFiles(new StaticFileOptions()
+    {
+        FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, staticPath)),
+        RequestPath = "/" + staticPath,
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseSession();
+app.UseSystemWebAdapters();
+app.MapHttpHandlers();
+app.MapScriptManager();
+app.MapBundleTable();
+
+app.Run();
